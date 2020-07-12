49b4b0acad782c1caae16348f9d26004511bd622
eenobai <67269852+eenobai@users.noreply.github.com>
2020-07-12 17:13:06 +0300

    initial

--- src/com/java/Services/Discounts.java
+++ src/com/java/Services/Discounts.java
@@ -2,6 +2,6 @@ package com.java.Services;
 
 public class Discounts {
 
-    //TODO maybe an automatic discount if the person meets a certain criteria?
+    //TODO maybe an automatic discount if the person meets a certain criteria?asd
 
 }
