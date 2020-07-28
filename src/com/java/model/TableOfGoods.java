4a69d10f3efe1889d9955e380bf0bbda4d74b809
eenobai <67269852+eenobai@users.noreply.github.com>
2020-07-28 09:01:07 +0300

    added null protection to sorter

--- src/com/java/model/TableOfGoods.java
+++ src/com/java/model/TableOfGoods.java
@@ -27,20 +27,35 @@ public class TableOfGoods {
     @Autowired
     SQLController sqlController;
 
-    public void showTable(String category, String sortBy) throws SQLException {
-        HashSet<String> waysToSort = new HashSet<String>(Arrays.asList("id", "goods_name", "price", "quantity"));
-        Statement statement = sqlController.sqlController().createStatement();
-        if(!waysToSort.contains(sortBy)){
-            System.out.println("wrong sort value");
-        }else {
-            ResultSet myRs = statement.executeQuery("SELECT * FROM " + category + " ORDER BY " + sortBy + ";");
-            while (myRs.next()) {
-                ids.add(myRs.getInt("id"));
-                goodsNames.add(myRs.getString("goods_name"));
+        public void sortingMess(String category, String sortBy) throws SQLException {
+            HashSet<String> waysToSort = new HashSet<String>(Arrays.asList("id", "goods_name", "price", "quantity"));
+            Statement statement = sqlController.sqlController().createStatement();
+            if (!waysToSort.contains(sortBy)) {
+              System.out.println("wrong sort value");
+            }else {
+                ResultSet myRs = statement.executeQuery("SELECT * FROM " + category + " ORDER BY " + sortBy + ";");
+                while (myRs.next()) {
+
+                    ids.add(myRs.getInt("id"));
+                 if (myRs.getString("goods_name") != null) {
+                    goodsNames.add(myRs.getString("goods_name"));
+                }else{
+                    goodsNames.add("String is a null");
+                    }
                 price.add(myRs.getFloat("price"));
                 quantity.add(myRs.getInt("quantity"));
-                tags.add(myRs.getString("tags"));
+                if (myRs.getString("tags") != null) {
+                    tags.add(myRs.getString("tags"));
+                }else{
+                    tags.add("String is a null");
+                    }
+                }
             }
+        }
+
+        public void showTable (String category, String sortBy) throws SQLException {
+            sortingMess(category, sortBy);
+
             String leftAlignFormat = "| %-6s | %-16s | %-5s | %-6d | %-31s |%n";
             System.out.println("################################################################################");
             System.out.println("#                       Existing items in database                             #");
@@ -48,18 +63,17 @@ public class TableOfGoods {
             System.out.format("+   ID   +     Item Name    + Price +Quantity+             Tags                +%n");
             System.out.format("+--------+------------------+-------+--------+---------------------------------+%n");
 
-            while(i<ids.size()){
+            while (i < ids.size()) {
                 System.out.format(leftAlignFormat, ids.get(i), goodsNames.get(i), price.get(i), quantity.get(i), tags.get(i));
                 i++;
             }
             System.out.format("+--------+------------------+-------+--------+---------------------------------+%n");
-        }
 
-        ids.clear();
-        goodsNames.clear();
-        price.clear();
-        quantity.clear();
-        tags.clear();
-        i = 0;
-    }
+            ids.clear();
+            goodsNames.clear();
+            price.clear();
+            quantity.clear();
+            tags.clear();
+            i = 0;
+        }
 }
